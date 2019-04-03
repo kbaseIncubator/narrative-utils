e40ef8dbcb3af595b9c1d7ea3e585b2d3ad8b8f8
Bill Riehl <wjriehl@lbl.gov>
2019-04-03 14:38:44 -0700

    better error checking of json rpc

--- src/jsonRpcClient.ts
+++ src/jsonRpcClient.ts
@@ -1,13 +1,3 @@
-// import {
-//     HttpClient,
-//     GeneralError,
-//     TimeoutError,
-//     AbortError,
-//     RequestOptions,
-//     HttpHeader
-// } from './HttpClient';
-// import * as Promise from 'bluebird';
-
 export interface KBaseJsonRpcRequestOptions {
     url: string,
     module: string,
@@ -34,17 +24,14 @@ export interface KBaseJsonRpcRequest {
 }
 
 export interface JsonRpcErrorInfo {
-    code: string,
-    status?: number,
+    code: number,
     message: string,
-    detail?: string
     data?: any
 }
 
-export class KBaseJsonRpcError extends Error {
-    code: string;
+export class KBaseJsonRpcError extends Error implements JsonRpcErrorInfo {
+    code: number;
     message: string;
-    detail?: string;
     data?: any;
     constructor(errorInfo: JsonRpcErrorInfo) {
         super(errorInfo.message);
@@ -53,7 +40,6 @@ export class KBaseJsonRpcError extends Error {
 
         this.code = errorInfo.code;
         this.message = errorInfo.message;
-        this.detail = errorInfo.detail;
         this.data = errorInfo.data;
         this.stack = (<any>new Error()).stack;
     }
@@ -62,10 +48,6 @@ export class KBaseJsonRpcError extends Error {
 export class KBaseJsonRpcClient {
     constructor() {
     }
-
-    // isGeneralError(error: GeneralError) {
-    //     return (error instanceof GeneralError);
-    // }
 
     request(options: KBaseJsonRpcRequestOptions) : Promise<any> {
         let rpcData : KBaseJsonRpcRequest = {
@@ -90,83 +72,25 @@ export class KBaseJsonRpcClient {
         };
         return fetch(options.url, request)
             .then(this.handleFetchErrors)
-            .then(response => response.json());
+            .then(response => response.json())
     }
 
     handleFetchErrors(response: any) {
-        if (!response.ok) {
-            throw Error(response.statusText);
+        if (response.ok) {
+            return response;
         }
-        return response;
+        return response.json()
+            .then((jsonError: any) => {
+                let detail = {};
+                if ('error' in jsonError) {
+                    detail = jsonError.error;
+                }
+                let info: JsonRpcErrorInfo = {
+                    code: response.status,
+                    message: response.statusText,
+                    data: detail
+                };
+                throw new KBaseJsonRpcError(info);
+            });
     }
-
-
-    // requestOld(options: KBaseJsonRpcRequestOptions) : Promise<any> {
-    //     let rpc : KBaseJsonRpcRequest = {
-    //         version: '1.1',
-    //         method: options.module + '.' + options.func,
-    //         id: String(Math.random()).slice(2),
-    //         params: options.params,
-    //     }
-    //     if (options.rpcContext) {
-    //         rpc.context = options.rpcContext;
-    //     }
-
-    //     let header : HttpHeader = new HttpHeader();
-    //     if (options.authorization) {
-    //         header.setHeader('authorization', options.authorization);
-    //     }
-
-    //     let requestOptions : RequestOptions = {
-    //         method: 'POST',
-    //         url: options.url,
-    //         timeout: options.timeout,
-    //         data: JSON.stringify(rpc),
-    //         header: header
-    //     }
-
-    //     let httpClient = new HttpClient();
-    //     console.log(requestOptions);
-    //     return httpClient.request(requestOptions)
-    //         .then(function (result) {
-    //             try {
-    //                 return JSON.parse(result.response);
-    //             } catch (ex) {
-    //                 throw new KBaseJsonRpcError({
-    //                     code: 'parse-error',
-    //                     message: ex.message,
-    //                     detail: 'The response from the service could not be parsed',
-    //                     data: {
-    //                         responseText: result.response
-    //                     }
-    //                 });
-    //             }
-    //         })
-    //         .catch(GeneralError, (err: GeneralError) => {
-    //             throw new KBaseJsonRpcError({
-    //                 code: 'connection-error',
-    //                 message: err.message,
-    //                 detail: 'An error was encountered communicating with the service',
-    //                 data: {}
-    //             });
-    //         })
-    //         .catch(TimeoutError, (err: TimeoutError) => {
-    //             throw new KBaseJsonRpcError({
-    //                 code: 'timeout-error',
-    //                 message: err.message,
-    //                 detail: 'There was a timeout communicating with the service',
-    //                 data: {}
-    //             });
-    //         })
-    //         .catch(AbortError, (err: AbortError) => {
-    //             throw new KBaseJsonRpcError({
-    //                 code: 'abort-error',
-    //                 message: err.message,
-    //                 detail: 'The connection was aborted while communicating with the service',
-    //                 data: {}
-    //             });
-    //         });
-
-    // }
-
 }
--- test/serviceClientSpec.js
+++ test/serviceClientSpec.js
@@ -3,15 +3,15 @@ const KBaseServiceClient = require('../lib/index.js').KBaseServiceClient;
 var expect = require('chai').expect;
 
 describe('KBase Service Client test', () => {
+    const servUrl = 'https://ci.kbase.us/services/service_wizard';
     beforeEach(() => {
         global.fetch = fetch;
     });
 
     it ('should do simple, non-authed lookups', (done) => {
-        let wizardClient = new KBaseServiceClient('ServiceWizard', 'https://ci.kbase.us/services/service_wizard', null);
+        let wizardClient = new KBaseServiceClient('ServiceWizard', servUrl, null);
         wizardClient.call('get_service_status', [{'module_name': 'NarrativeService', 'version': null}])
             .then((result) => {
-                console.log(result);
                 expect(result.url).to.contain('https://ci.kbase.us');
                 expect(result.url).to.contain('NarrativeService');
                 done();
@@ -22,5 +22,21 @@ describe('KBase Service Client test', () => {
             });
     });
 
-
+    it ('should fail reasonably when given bad parameters', (done) => {
+        let wizardClient = new KBaseServiceClient('ServiceWizard', servUrl, null);
+        wizardClient.call('get_service_status', [{'module_name': 'NarrativeService'}])
+            .then(() => {
+                done(new Error('Should have failed!'));
+            })
+            .catch((err) => {
+                expect(err.message).to.equal('Internal Server Error');
+                expect(err.code).to.equal(500)
+                expect(err.data).to.deep.contain({
+                    message: 'version',
+                    code: -32000,
+                    name: 'Server error'
+                })
+                done();
+            });
+    });
 });
