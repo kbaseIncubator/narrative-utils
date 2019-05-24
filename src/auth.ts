7ec83ca79da7074d3a3fb1211a171fb77fe8e78f
Tianhao-Gu <tgu@anl.gov>
2019-05-24 00:32:05 -0500

    add get token info helper func

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,38 @@ import {
     NarrativeConfig
 } from './config';
 
+export interface AuthErrorInfo {
+    code: number,
+    message: string,
+    data?: any
+}
+
+export class AuthError extends Error implements AuthErrorInfo {
+    /**
+     * The HTTP error code (likely 500, because that's the JSON-RPC stack we have).
+     */
+    code: number;
+    /**
+     * The primary message from the service.
+     */
+    message: string;
+    /**
+     * An optional data stack provided by the service with more error details. Typically
+     * contains a server-side stacktrace, error code, and detailed message.
+     */
+    data?: any;
+    constructor(errorInfo: AuthErrorInfo) {
+        super(errorInfo.message);
+        Object.setPrototypeOf(this, AuthError.prototype);
+        this.name = 'AuthError';
+
+        this.code = errorInfo.code;
+        this.message = errorInfo.message;
+        this.data = errorInfo.data;
+        this.stack = (<any>new Error()).stack;
+    }
+}
+
 /**
  * Handles the token-based KBase authentication stuff. An Auth token is fetched from
  * the browser's "kbase_session" cookie (by default - see NarrativeConfig), stored in this object,
@@ -11,7 +43,7 @@ export class Auth {
     config: NarrativeConfig;
 
     /**
-     * The user's auth token.
+     * The user's auth token and user id.
      */
     token: string = null;
 
@@ -42,5 +74,77 @@ export class Auth {
         else {
             return null;
         }
+    }
+
+    /* does a GET request to fetch a token's introspection.
+     * Returns a json doc with these keys:
+     * expires - millis since epoch
+     * created - millis since epoch
+     * name - if the token has a name
+     * id - a UUID for that token
+     * type - Login, etc.
+     * user - KBase user id
+     * cachefor - millis
+     */
+    getTokenInfo(token?: string) {
+        return this.makeAuthCall(token ? token : this._getTokenFromCookie(), {
+            operation: '/token',
+            method: 'GET'
+        });
+    }
+
+    /**
+     * returns a Promise that makes the API call.
+     * the call that gets made looks like:
+     * Configured URL + '/api/' + version + '/' + method
+     * the "method" string should contain all url encoded
+     * parameters as expected.
+     */
+    makeAuthCall(token: string, callParams: {operation: string, method: string, version?: string}) {
+        let version = callParams.version || 'V2';
+        let call_url = [
+                this.config.urls.auth,
+                '/api/',
+                version,
+                callParams.operation
+            ].join('');
+
+        let request : RequestInit = {
+            method: callParams.method,
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': token
+            }
+        };
+
+        return fetch(call_url, request)
+            .then(this.handleFetchErrors)
+            .then(response => response.json())
+    }
+
+    handleFetchErrors(response: any) {
+        if (response.ok) {
+            return response;
+        }
+        return response.text()
+            .then((errText: string) => {
+                let detail: any = {};
+                try {
+                    detail = JSON.parse(errText);
+                    console.log(detail);
+                    if ('error' in detail) {
+                        detail = detail['error'];
+                    }
+                }
+                catch {
+                    detail['message'] = errText;
+                }
+                let info: AuthErrorInfo = {
+                    code: response.status,
+                    message: response.statusText,
+                    data: detail
+                };
+                throw new AuthError(info);
+            });
     }
 }
